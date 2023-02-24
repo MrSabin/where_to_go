9464c47fb68c8581b02fb89eba97593fe49120b6
MrSabin <sabin.21011986@gmail.com>
2023-02-24 17:59:53 +0300

    Got data from DB

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,30 +1,30 @@
 from django.shortcuts import render
 
+from places.models import Place
+
 
 def render_main(request):
+    features = []
+    detail_urls = ["static/places/moscow_legends.json", "static/places/roofs24.json"]
+    places = Place.objects.all()
+
+    for place in places:
+        feature = {
+            "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [place.lon, place.lat]},
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": detail_urls[place.id - 1],
+            },
+        }
+        features.append(feature)
+
     context = {
         "places": {
             "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "geometry": {"type": "Point", "coordinates": [37.62, 55.793676]},
-                    "properties": {
-                        "title": "«Легенды Москвы",
-                        "placeId": "moscow_legends",
-                        "detailsUrl": "static/places/moscow_legends.json",
-                    },
-                },
-                {
-                    "type": "Feature",
-                    "geometry": {"type": "Point", "coordinates": [37.64, 55.753676]},
-                    "properties": {
-                        "title": "Крыши24.рф",
-                        "placeId": "roofs24",
-                        "detailsUrl": "static/places/roofs24.json",
-                    },
-                },
-            ],
+            "features": features,
         }
     }
+
     return render(request, "index.html", context)
