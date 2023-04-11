db2b4bba3955b35c224101e74d8c448315560e05
MrSabin <sabin.21011986@gmail.com>
2023-04-11 21:46:29 +0300

    Added images preview to admin inline

--- places/admin.py
+++ places/admin.py
@@ -1,15 +1,33 @@
+import traceback
+
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Image, Place
 
 
 class ImageInline(admin.TabularInline):
     model = Image
-    raw_id_fields = ("place", )
+    raw_id_fields = ("place",)
+    readonly_fields = ["place_image"]
+
+    def place_image(self, obj):
+        return format_html(
+            '<img src="{}" style="max-width:{};max-height:{};width:{};height:{};" />',
+            obj.image.url,
+            "200px",
+            "200px",
+            "auto",
+            "auto",
+        )
+
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ["title"]
     inlines = [ImageInline]
 
-admin.site.register(Image)
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ["image"]
