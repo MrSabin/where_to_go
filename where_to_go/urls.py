b478a3fa6d0db72fb9781a62fb5091d734caadcc
MrSabin <sabin.21011986@gmail.com>
2023-04-13 20:26:40 +0300

    Added HTML editor to description field

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from where_to_go import views
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.render_main),
     path("places/<int:place_id>/", views.get_place_meta, name="place_meta"),
+    path("tinymce/", include("tinymce.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
